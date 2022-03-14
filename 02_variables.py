db69c7d4ba8023b02c2e072ec3f31c2d74526a3e
emmottj2245 <101373326+emmottj2245@users.noreply.github.com>
2022-03-15 12:20:48 +1300

    Add files via upload
    wrote my fav number, my name

---
+++ 02_variables.py
@@ -0,0 +1,14 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num = 7
+
+# this variable has a problem
+my_name = "jack emmott"
+
+print(greeting)
+print(fav_num)
+print(my_name)

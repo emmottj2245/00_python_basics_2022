e7372ef00c345c5b21177bd31ad1636147c0fbb5
emmottj2245 <101373326+emmottj2245@users.noreply.github.com>
2022-03-25 14:17:08 +1300

    Add files via upload
    my code for names and favourite number

--- 03_getting_input.py
+++ 03_getting_input.py
@@ -3,4 +3,9 @@ fav_num = int(input("favourite number?"))
 double = fav_num * 2
 half = fav_num / 2
 squared = fav_num * fav_num
+print()
 print("hi {}, you favourite number is {}".format(username, fav_num))
+print("double {} is {}".format(fav_num, double))
+print("half {} is {}".format(fav_num, half))
+print("{} squared is {}" .format(fav_num, squared))
+print()
